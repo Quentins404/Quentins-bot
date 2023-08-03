b906b78e64694e18912b19a79dfa01972be638a5
Quentins84004 <138402819+Quentins84004@users.noreply.github.com>
2023-08-03 22:03:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,17 @@ async def on_message(message):
 
     # this runs code every time a message is sent
     # await message.channel.send("A message was just sent here!")
-    # the ID for the channel you want to use (I swapped it for testing): 1128136575776198676 #can we change it ? what? the id
+    # the ID for the channel you want to use (I swapped it for testing): 1128136575776198676 
 
     if message.content.startswith("/status"):
-        otherChannel = discord.utils.get(message.guild.channels, id=1128136575776198676)
+        otherChannel = discord.utils.get(message.guild.channels, id= THE CHANNEL ID)
         print(otherChannel)
         print(type(otherChannel))
         lastMessage = ""
         async for searchMessage in otherChannel.history(limit=1):
             lastMessage = searchMessage.content
 
-        await message.channel.send(f"The server is {lastMessage}")
+        await message.channel.send(f"your message here {lastMessage}")
 
 
 client.run('')
